94adc714c2cf18b19f1da531c1c8691ff9b08c9d
Dan Sm <dsmajdorr@gmail.com>
2022-04-22 00:08:23 +0200

    Revert "Revert "DepartmentDTO.EmployeeCount parameter added for within views without initializing any employee objects""
    This reverts commit 8ad64e1fd69dcaa007284c7729b940b0885ff1ee.

--- CompanyApp/DTOs/DepartmentDTO.cs
+++ CompanyApp/DTOs/DepartmentDTO.cs
@@ -11,7 +11,10 @@ namespace CompanyApp.DTOs
         public string? Name { get; set; }
         public string? Location { get; set; }
         public int? BudgetYearly { get; set; }
+        
         public ICollection<EmployeeDTO> Employees { get; set; }
+        public int EmployeeCount { get; private set; }
+
 
         public DepartmentDTO()
         {
@@ -25,17 +28,8 @@ namespace CompanyApp.DTOs
             this.Name = department.Name;
             this.Location = department.Location;
 
-           // this.Employees = mapEmployees(department.Employees).ToList();
-
-            //if (department.Employees != null) { }
-                //this.Employees = department.Employees.Select(e => new EmployeeDTO(e)).ToList<EmployeeDTO>(); //this line crashes the server 
+            if (department.Employees != null) this.EmployeeCount = department.Employees.Count;
+            else this.EmployeeCount = 0;
         }
-
-        //private IEnumerable<EmployeeDTO> mapEmployees(ICollection<Employee> employees)
-        //{
-
-        //    foreach (var emp in employees.ToList())
-        //        yield return new EmployeeDTO(emp);
-        //}
     }
 }
